1d728a4690440135ed6d10caf5503b9117821e65
Tudor Roman <tudor@tudorr.ro>
2025-06-09 13:48:18 +0200

    Fix build

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,12 +9,14 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf5 copr enable pgdev/ghostty
+dnf5 copr -y enable pgdev/ghostty
 
 # Install various programs
-dnf5 install -y fish brightnessctl ghostty htop libreoffice niri swaybg swayidle swaylock waybar wlsunset xlsclients xwayland-satellite virt-manager
+dnf5 install -y fish brightnessctl htop libreoffice niri swaybg swayidle swaylock waybar wlsunset xlsclients xwayland-satellite virt-manager
+# dnf5 install -y ghostty
 
 # Install 1password
+mkdir /var/opt
 rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
 dnf5 install -y 1password
